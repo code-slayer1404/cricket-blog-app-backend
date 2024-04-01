7ec20c4041ec35a05ee3c3ff85b1683ac4dbe613
Pranshu Sinha <pranshu.yashuuma@gmail.com>
2024-04-02 04:51:48 +0530

    configured cors basic

--- src/main/java/com/pranshu/blogapp/entity/User.java
+++ src/main/java/com/pranshu/blogapp/entity/User.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -29,6 +30,7 @@ public class User {
     private int id;
 
     private String name;
+    @Column(unique = true)
     private String username;
     private String password;
 
