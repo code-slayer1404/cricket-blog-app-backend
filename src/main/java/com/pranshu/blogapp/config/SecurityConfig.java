5c5a91f8bc66d446a62c71a0276e899c89fdfa6c
Pranshu Sinha <pranshu.yashuuma@gmail.com>
2025-11-12 22:57:23 +0530

    test

--- src/main/java/com/pranshu/blogapp/config/SecurityConfig.java
+++ src/main/java/com/pranshu/blogapp/config/SecurityConfig.java
@@ -86,7 +86,7 @@ public class SecurityConfig {
     public CorsConfigurationSource getCorsConfigurationSource(){
         CorsConfiguration configuration = new CorsConfiguration();
         // configuration.setAllowedOriginPatterns(Arrays.asList("http://localhost:[5173]"));
-        // configuration.setAllowedOriginPatterns(Arrays.asList("https://code-slayer1404.github.io","http://blogappfrontendbucket.s3-website.ap-south-1.amazonaws.com","http://localhost:[5173]"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("https://code-slayer1404.github.io","http://blogappfrontendbucket.s3-website.ap-south-1.amazonaws.com","http://localhost:[5173]"));
         configuration.setAllowedOrigins(Arrays.asList("https://code-slayer1404.github.io","http://blogappfrontendbucket.s3-website.ap-south-1.amazonaws.com","http://localhost:[5173]"));
         configuration.setAllowedMethods(Arrays.asList("*"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
